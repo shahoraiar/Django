eb17395136fa3617f896c8fbc46cc410ecf4dfcd
shahoraiar <shahoraiar2000@gmail.com>
2023-11-13 15:08:30 +0600

    first commit

--- Django_Mart/Django_Mart/urls.py
+++ Django_Mart/Django_Mart/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home , name='home'),
     path('account/',include('accounts.urls')),
     path('cart/',include('cart.urls')),
     # path('category/',include('category.urls')),
