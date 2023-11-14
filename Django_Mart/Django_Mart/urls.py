186ee88e7d7d88dea415015f3e5d37110b0d7a44
shahoraiar <shahoraiar2000@gmail.com>
2023-11-14 16:52:52 +0600

    first commit

--- Django_Mart/Django_Mart/urls.py
+++ Django_Mart/Django_Mart/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from . import views
-
+from . import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home , name='home'),
@@ -26,4 +27,4 @@ urlpatterns = [
     # path('category/',include('category.urls')),
     path('order/',include('orders.urls')),
     path('store/',include('store.urls')),
-]
+] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
