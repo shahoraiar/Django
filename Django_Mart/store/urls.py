e728410551787e0e83816f71dd2df28116a40c6c
shahoraiar <shahoraiar2000@gmail.com>
2023-11-16 10:27:17 +0600

    second commit

--- Django_Mart/store/urls.py
+++ Django_Mart/store/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('',views.store , name='store'), #store
+    path('category/<slug:category_slug>/', views.store , name='product_by_category'), #store
     path('product-details/',views.product_detail , name='product-details')
+    
 ]
--- Django_Mart/store/views.py
+++ Django_Mart/store/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from category.models import Product , Category
 # Create your views here.
 
-def store(request) : 
+def store(request , category_slug=None) : 
+    categorie = get_object_or_404(Category , slug = category_slug)
+    print(categorie)
     product = Product.objects.filter(is_available = True)
     # print(product)
     category = Category.objects.all()
