3db0f1aaedada4fe0d1287654dbbc2fa291552a4
shahoraiar <shahoraiar2000@gmail.com>
2023-11-27 10:59:52 +0600

    second commit

--- Django_Mart/cart/views.py
+++ Django_Mart/cart/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from category.models import Product
 # Create your views here.
 
 def cart(request) : 
     return render(request , 'cart/cart.html')
 
+def add_to_cart(request , product_id) : 
+    product = Product.objects.get(id = product_id)
+    
