16f4298459518e7df5e5c82f1a1c8184f77e7323
shahoraiar <shahoraiar2000@gmail.com>
2023-11-29 16:28:32 +0600

    3rd commit

--- Django_Mart/accounts/urls.py
+++ Django_Mart/accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('register/', views.register , name='register'), #accounts/
     path('profile/', views.profile , name='profile'),
     path('signin/', views.signin , name='signin'),
+    path('logout/', views.user_logout , name='logout'),
 ]
--- Django_Mart/accounts/views.py
+++ Django_Mart/accounts/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
+from django.contrib.auth import login,logout,authenticate
 # Create your views here.
 
 def register(request) : 
-    return render(request , 'accounts/register.html')
+    form = RegisterForm()
+    if request.method == 'POST' : 
+        form = RegisterForm(request.POST)
+        if form.is_valid() : 
+            user = form.save()
+            login(request , user)
+            return redirect('profile')
+    return render(request , 'accounts/register.html' , {'form' : form})
 
 def profile(request) : 
     return render(request , 'accounts/dashboard.html')
 
-def signin(request) : 
+def signin(request) : #login
+    if request.method == 'POST' : 
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = user_name , password = password)
+        # print(user)
+        if user: 
+            login(request , user)
+            return redirect('profile')
     return render(request , 'accounts/signin.html')
+
+def user_logout(request) : 
+    logout(request)
+    return redirect('signin')
