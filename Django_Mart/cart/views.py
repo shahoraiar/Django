234ccf57fc073b65a9830fa492115dac4a080d44
shahoraiar <shahoraiar2000@gmail.com>
2023-11-29 14:11:08 +0600

    3rd commit

--- Django_Mart/cart/views.py
+++ Django_Mart/cart/views.py
@@ -4,12 +4,31 @@ from cart.models import Cart , CartItem
 # Create your views here.
 
 def cart(request) : 
-    return render(request , 'cart/cart.html')
+    session_id = request.session.session_key #session id nie aslam
+    modelid = Cart.objects.get(cart_id = session_id) # model anlam
+    cart_id = Cart.objects.filter(cart_id = session_id).exists() #session id database e ace ki na
+    cart_item = None
+    tax = 0 
+    total = 0
+    grand_total = 0
+
+    if cart_id : 
+        cart_item = CartItem.objects.filter(cart = modelid)
+        for item in cart_item : 
+            total += item.product.price * item.quantity
+    tax = (2*total)/100 # 2% vat
+    grand_total = total + tax
+    return render(request , 'cart/cart.html' , {'cart_item' : cart_item , 'total': total, 'tax':tax,
+                                                'grand_total' : grand_total ,})
 
 def add_to_cart(request , product_id) : 
     product = Product.objects.get(id = product_id)
     # print('add to cart : ' ,product)
-    session_id = request.session.session_key #session id
+    # session_id = request.session.session_key #session id
+    if not request.session.session_key:
+        request.session.create()
+
+    session_id = request.session.session_key
     # print('session : ',cart)
     cart_id = Cart.objects.filter(cart_id = session_id).exists()
     if cart_id : 
@@ -33,4 +52,26 @@ def add_to_cart(request , product_id) :
         cart.save()
      
     return redirect('cart')
+
+def decrease_cart(request , product_id) :
+    # print('id : ',product_id)
+    session_id = request.session.session_key
+    modelid = Cart.objects.get(cart_id = session_id) #cartid/model search
+    product = Product.objects.get(id = product_id)
+    cart_item = CartItem.objects.get(cart = modelid , product = product) #cartitem filter
+    if cart_item.quantity > 1 : 
+        cart_item.quantity -= 1
+        cart_item.save()
+    else : 
+        cart_item.delete()
+
+    return redirect('cart')
     
+def remove(request , product_id) : 
+    session_id = request.session.session_key
+    modelid = Cart.objects.get(cart_id = session_id) #cartid/model search
+    product = Product.objects.get(id = product_id)
+    cart_item = CartItem.objects.get(cart = modelid , product = product) #cartitem filter
+    cart_item.delete()
+
+    return redirect('cart')
